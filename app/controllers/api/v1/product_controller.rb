f6a502946506fd12864372f14a544e128c62554c
ToddRizley <todd.rizley@gmail.com>
2016-09-30 20:57:54 -0400

    set up product controller

--- app/controllers/api/v1/product_controller.rb
+++ app/controllers/api/v1/product_controller.rb
@@ -1,2 +1,30 @@
-class ProductController < ApplicationController
+module Api
+  module V1
+    class ProductController < ApplicationController
+
+      def create
+        @product =  Product.create(product_params)
+        render json: @product
+      end
+
+      def index
+        render json: Product.all
+      end
+
+      def update
+        @product = Product.find_by(product_params["title"])
+
+        render json: @product
+      end
+
+      def destroy
+      end
+
+      private
+
+      def product_params
+        params.require(:product).permit(:title, :quantity, :selected, :price)
+      end
+    end
+  end
 end
